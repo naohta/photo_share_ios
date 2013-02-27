553f4d1858a660d31f0a3289ac72c7880c273bb7
Naohiro OHTA <naohta@gmail.com>
2013-02-27 10:30:42 +0900

    WOOORRKK! But cant compless

--- photo_share/photo_share/ViewController.m
+++ photo_share/photo_share/ViewController.m
@@ -3,6 +3,8 @@
 #import "ViewController.h"
 #import "R9HTTPRequest.h"
 #import "AppDelegate.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <ImageIO/ImageIO.h>
 @interface ViewController ()
 @property(strong,nonatomic) NSMutableData* receivedOne;
 @end
@@ -31,7 +33,16 @@
     [self presentViewController:picker animated:YES completion:^(){}];
 }
 
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+/*
+-(IBAction)takeButton:(id)sender {
+    UIImagePickerController *picker = [[[UIImagePickerController alloc] init] autorelease];
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    picker.delegate = self;
+    [self presentModalViewController:picker animated:YES];
+}
+*/
+
+- (void)imagePickerController3:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
     NSLog(@"%s",__func__);
     [picker dismissViewControllerAnimated:YES completion:^(){
@@ -40,8 +51,6 @@
         NSData* imgData = [[NSData alloc]initWithData:UIImageJPEGRepresentation(img,0.0)];
         NSLog(@"%@",@"imagData complete");
         
-        //NSURL *URL = [NSURL URLWithString:@"http://Naos-Air11-Mid-2012.local:4567/photo"];
-        //NSURL *URL = [NSURL URLWithString:@"http://photo.elasticbeanstalk.com/photo"];
         NSURL *URL = [NSURL URLWithString:_url_strings[_url_string_index]];
         R9HTTPRequest *request = [[R9HTTPRequest alloc] initWithURL:URL];
         [request setHTTPMethod:@"POST"];
@@ -61,11 +70,84 @@
     }];
 }
 
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSURL *assetURL = [info objectForKey:UIImagePickerControllerReferenceURL];
+    
+    if (assetURL) {
+        ALAssetsLibraryAssetForURLResultBlock resultBlock = ^(ALAsset *asset) {
+            /*
+            ALAssetRepresentation *representation;
+            representation = [asset defaultRepresentation];
+            // 元から付いているメタデータも残す
+            NSMutableDictionary *metadata;
+            metadata = [[NSMutableDictionary alloc]
+                        initWithDictionary:[representation metadata]];
+            
+            // コメント、シャッター速度をExif情報として設定
+            NSDictionary *exif = [NSDictionary dictionaryWithObjectsAndKeys:
+                                  @"コメント",
+                                  (NSString*)kCGImagePropertyExifUserComment,
+                                  [NSNumber numberWithFloat:0.125f],
+                                  (NSString*)kCGImagePropertyExifExposureTime,
+                                  nil];
+            [metadata setObject:exif
+                         forKey:(NSString*)kCGImagePropertyExifDictionary];
+            
+            // カメラロールにメタデータを付けて書き込み
+            ALAssetsLibrary* l = [[ALAssetsLibrary alloc] init];
+            [l writeImageToSavedPhotosAlbum:[representation fullScreenImage]
+                                   metadata:metadata
+                            completionBlock:^(NSURL* url, NSError* e){
+                                NSLog(@"Saved: %@<%@>", url, e);
+                            }];
+            */
+            ALAssetRepresentation *rep = [asset defaultRepresentation];
+            Byte *buffer = (Byte*)malloc(rep.size);
+            NSUInteger buffered = [rep getBytes:buffer fromOffset:0.0 length:rep.size error:nil];
+            NSData* imgData = [NSData dataWithBytesNoCopy:buffer length:buffered freeWhenDone:YES];//this is NSData may be what you want
+
+            NSURL *URL = [NSURL URLWithString:_url_strings[_url_string_index]];
+            R9HTTPRequest *request = [[R9HTTPRequest alloc] initWithURL:URL];
+            [request setHTTPMethod:@"POST"];
+            [request addBody:@"test" forKey:@"TestKey"];
+            [request setData:imgData withFileName:@"nao.jpg" andContentType:@"image/jpg" forKey:@"imgData"];
+            [request setCompletionHandler:^(NSHTTPURLResponse *responseHeader, NSString *responseString){
+                NSLog(@"%@", responseString);
+                self.textView.text = responseString;
+            }];
+            // Progress
+            [request setUploadProgressHandler:^(float newProgress){
+                NSLog(@"%g", newProgress);
+                self.textView.text = [NSString stringWithFormat:@"Progress.. %f, now.",newProgress*100];
+            }];
+            [request startRequest];
+            
+        };
+        
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        [library assetForURL:assetURL
+                 resultBlock:resultBlock
+                failureBlock:^(NSError *error) {
+                    NSLog(@"error:%@", error);
+                }];
+    } else { NSLog(@"error"); }
+}
+
+
+
+
+
+
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
 
